6045c7a2a66f007238b145918faf3fbc7495fcb3
TodoH <hero.td@gmail.com>
2016-10-31 07:24:33 +0900

    last_idを更新して未取得をリクエストするように変更

--- static/scripts/brython/script.py
+++ static/scripts/brython/script.py
@@ -8,15 +8,29 @@ json = window.JSON
 #import json
 jq = window.jQuery
 b64 = window.Base64
+last_id = -1;
 #print(b64.encode("あああ"))
 def on_complete(req):
 #  print(req.text)
 #      for i in range(0,15):
+  global last_id
   data = json.parse(str(req.text))
-  print(data['ID'])
-#  for i in data:
-#    print(i)
-  doc["contents_list"].html += "<li>" +(req.text) + "</li>"
+  print(len(data['item']))
+  for i in range(0,len(data['item'])-1):
+#    print(data['item'][i].ID)
+    add_html = "<li>"
+    add_html += "<h2>" + data['item'][i].main_comment + "</h2>"
+    add_html += "<p>" + data['item'][i].url + "</p>"
+    add_html += "<p>" + data['item'][i].sub_comment + "</p>"
+    add_html += "<p>shikoiine:" + str(data['item'][i].shikoiine) + "</p>"
+    add_html += "<p>naerune:" + str(data['item'][i].naerune) + "</p>"
+    add_html += "<p>guilty:" + str(data['item'][i].guilty) + "</p>"
+    add_html += "<p>ID:" + str(data['item'][i].ID) + "</p>"
+    last_id = data['item'][i].ID
+    for tag_index in range(0,len(data['item'][i].tag)):
+      add_html += "<p>" + data['item'][i].tag[tag_index] + "</p>"
+    add_html += "</li>"
+    doc["contents_list"].html += add_html
 
 #    if req.status==200 or req.status==0:
 #        doc["result"].html = req.text
@@ -55,10 +69,11 @@ def get(url,param={}):
 #      print(i)
 
 def callback(event, isInView):
+  global last_id
   if(isInView):
     print("見えた")
 #    print( base64.b64encode("見えた"))
-    get("http://t0d0.jp:8889/api/",{"id":"-1","tag":["-1"]})#-1でnone指定
+    get("http://t0d0.jp:8889/api/",{"id":last_id,"tag":["-1"]})#-1でnone指定
 #    contents_list.appendChild()
 #    for i in range(0,15):
 #      doc["contents_list"].html += "<li>test</li>"
