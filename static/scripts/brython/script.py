cf1420d6a99a50e8e3671a633e8a7d60673a713a
TodoH <hero.td@gmail.com>
2016-11-01 20:23:24 +0900

    ボタン周りの処理 ev.targetがthisに相当

--- static/scripts/brython/script.py
+++ static/scripts/brython/script.py
@@ -6,8 +6,10 @@ json = window.JSON
 jq = window.jQuery
 b64 = window.Base64
 last_id = -1;
+content_index = 0
 def on_complete(req):
   global last_id
+  global content_index
   data = json.parse(str(req.text))
   print(len(data['item']))
   for i in range(0,len(data['item'])-1):
@@ -15,15 +17,18 @@ def on_complete(req):
     add_html += "<h2>" + data['item'][i].main_comment + "</h2>"
     add_html += "<p>" + data['item'][i].url + "</p>"
     add_html += "<p>" + data['item'][i].sub_comment + "</p>"
-    add_html += "<p>shikoiine:" + str(data['item'][i].shikoiine) + "</p>"
-    add_html += "<p>naerune:" + str(data['item'][i].naerune) + "</p>"
-    add_html += "<p>guilty:" + str(data['item'][i].guilty) + "</p>"
+    add_html += "<p class = 'shikoiine' id = '"+str(data['item'][i].ID)+"'>shikoiine:" + str(data['item'][i].shikoiine) + "</p>"
+    add_html += "<p class = 'naerune' id = '"+str(data['item'][i].ID)+"'>naerune:" + str(data['item'][i].naerune) + "</p>"
+    add_html += "<p class = 'guilty' id = '"+str(data['item'][i].ID)+"'>guilty:" + str(data['item'][i].guilty) + "</p>"
     add_html += "<p>ID:" + str(data['item'][i].ID) + "</p>"
     last_id = data['item'][i].ID
     for tag_index in range(0,len(data['item'][i].tag)):
       add_html += "<p>" + data['item'][i].tag[tag_index] + "</p>"
     add_html += "</li>"
     doc["contents_list"].html += add_html
+    jq(".shikoiine").on("click",post)
+    
+    content_index += 1
 
 #    if req.status==200 or req.status==0:
 #        doc["result"].html = req.text
@@ -54,6 +59,17 @@ def get(url,param={}):
     req.open('GET',send_param,True)
     req.send()
 
+def post(ev):
+  
+#    req = ajax.ajax()
+#    req.bind('complete',on_complete)
+#    req.set_timeout(timeout,err_msg)
+#    send_param = url
+#    req.open('GET',send_param,True)
+#    req.send()
+  print(jq(ev.target).attr('id'))
+#  print("shiko")
+
 def callback(event, isInView):
   global last_id
   if(isInView):
@@ -64,3 +80,15 @@ def callback(event, isInView):
     print("消えた")
 
 jq('#last').on('inview', callback)
+#document["shikotta-button"].bind('click',post)
+#document["shikotta"+"-button"].unbind()
+#document["shikotta-button"].bind('click',post)
+
+
+jq('.shikotta-button').on('click',post)
+
+#def callback(ev);
+#  print(ev.target)
+
+#jq('.shikotta-button').on('click',callback)
+#jq('.shikotta-button').on('click',lambda event:print(jq(event.target).attr('id')))
