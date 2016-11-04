abb7dd3c97c1012cafdd41b65342ad7856eb6d64
t0d0 <hero.td@gmail.com>
2016-11-04 23:19:07 +0900

    何故かPOST受け取れなくなったので修正。

--- server.py
+++ server.py
@@ -15,20 +15,6 @@ db = DB_Access()
 
 global_text = ""
 server = None
-  
-#  def put(self, *args, **kwargs):
-##    raise HTTPError(405)
-#  self.set_status(400)
-#
-#    print("konsutorakuta")
-#  def put(self, *args, **kwargs):
-#    pass
-#    print("put is done")
-
-#    pass
-#    self.conn = connection
-#    self.db = self.conn['blogger']
-#    self.collection = self.db['blogs']
 
 class MainHandler(tornado.web.RequestHandler):
 
@@ -44,7 +30,6 @@ def args_to_dict(args):
   answer = {'id':'','tag':[]}
   work = re.split(',',args)
   i=0
-#  print(base64.decodestring(work[i+1].encode("ascii")).decode("utf8"))
   while(i<len(work)):
     if(work[i]=="num"):
       answer['num'] =int(base64.decodestring(work[i+1].encode("ascii")).decode("utf8"))
@@ -62,7 +47,6 @@ class APIHandler(tornado.web.RequestHandler):
   def get(self,*args, **kwargs):
 
     convert_data = args_to_dict(kwargs['args'])
-#    print(convert_data['id'])
     None_flag = False
     write_text = '{"item":['
     for i in db.get_list(ID = int(convert_data['id']),tag = convert_data['tag'],num = convert_data["num"]):
@@ -78,26 +62,21 @@ class APIHandler(tornado.web.RequestHandler):
     self.finish()
     
   @tornado.web.asynchronous
-  def post(self):
-#    pass
-#    print("testdayooooooooooooooooo")
-#    self.write("test")
-#    text = self.get_argument('main_comment')
-#    self.write(text)
+  def post(self, *args, **kwargs):
     data = {"main_comment":base64.decodestring(self.get_argument('main_comment').encode("ascii")).decode("utf-8"),
      "sub_comment":base64.decodestring(self.get_argument('sub_comment').encode("ascii")).decode("utf-8"),
      "url":base64.decodestring(self.get_argument('url').encode("ascii")).decode("utf-8"),
      "tag":re.split(',',base64.decodestring(self.get_argument('tag').encode("ascii")).decode("utf-8"))
            }
-##    print(base64.decodestring(self.get_argument('tag').encode("ascii")).decode("utf-8"))
-#    for i in re.split(',',base64.decodestring(self.get_argument('tag').encode("ascii")).decode("utf-8")):
-#      print(i)
+    
     db.set_data(data)
     self.finish()
-
+    
+  @tornado.web.asynchronous
   def put(self, *args, **kwargs):
     ID = kwargs['args']
     action = self.get_argument('action', None)
+    print(action)
     if(action == "shikoiine"):
       db.update_shikoiine(int(ID))
       
@@ -107,15 +86,32 @@ class APIHandler(tornado.web.RequestHandler):
     if(action == "guilty"):
       db.update_guilty(int(ID))
     
-#    print(self.)
-
+#    ans = ""
+    write_text = '{"item":['
+    for i in db.get_data(int(ID)):
+      write_text += str(i)
+    print(write_text)
+    
+    
+    None_flag = False
+    write_text = '{"item":['
+    for i in db.get_data(int(ID)):
+#      print(i)
+      None_flag = True
+      write_text += (str(i).replace("\'","\"")+",")
+    write_text = write_text[:-1]#最後のカンマを除去
+    write_text += ']}'
+    print(write_text)
+    self.write(write_text)
+    self.finish()
+    
 #実行用関数
 def serve_forever():
   global server
   application = tornado.web.Application([
     (r"/", MainHandler),
-    (r"/api", APIHandler),
     (r"/api/(?P<args>.+)", APIHandler),
+    (r"/api", APIHandler),
     (r"/api/?(.*)", APIHandler),
     
     (r"/new_content", NewContentHandler),
