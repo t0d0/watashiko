e0201b72295352cb64deb393f7307be2fa8cbb24
t0d0 <hero.td@gmail.com>
2016-11-06 12:27:37 +0900

    画像をハッシュしてnow_printingの画像のhash値と比較、一致したら再取得

--- get_thumbnail.py
+++ get_thumbnail.py
@@ -1,10 +1,14 @@
 #!/bin/env python
 # -*- coding: utf-8 -*-
 import urllib
+from struct import *
 #from datetime import datetime
 import hashlib
 import os
 import threading
+import time
+NG_hash = "72d8ecec1defbfc3acb50c67dea9eb88"
+
 
 
 class GetThumbnailThread(threading.Thread):
@@ -17,20 +21,51 @@ class GetThumbnailThread(threading.Thread):
   
   def run(self):
     while(1):
-      if(len(self.task)>=0):
+
+      if(len(self.task)>0):
         try:
           next_task = self.task.pop()
           os.system("mkdir " + next_task["path"])
           os.system("wget -O " + next_task["path"]+ "/" +next_task["name"] + " http://capture.heartrails.com/huge?" + next_task["url"])
+          next_task["path"]+ "/" +next_task["name"]
+#          if(check_NG(next_task["path"]+ "/" +next_task["name"])):
+#            print("img_ok")
+#            pass
+#          else:
+#            print("img_ng")
+#            self.task.append(next_task)
+
         except:
+          print("コマンド実行エラー")
           pass
+        finally:
+          if(self.check_NG(next_task["path"]+ "/" +next_task["name"])):
+            print("img_ok")
+            pass
+          else:
+            print("img_ng")
+            self.task.append(next_task)
+
+          
+      time.sleep(2)
   
   def append_task(self,path,file_name,url):
     self.task.append({"path":path,"name":file_name,"url":url})
+  
+  def check_NG(self,path):
+    print("img_checking...")
+    bin_data = open(path,'rb')
+    string = ""
+    while(1) :
+        b = bin_data.read(1)
+        if b == "" :
+            break
+        try:
+          string+=str(unpack('c',b))
+        except:
+          break
 
-#if __name__ == '__main__':
-#    thumb = GetThumbnailThread()
-#    thumb.start()
-#    thumb.append_task("/var/www/tornado/test1.jpg","http://google.com")
-#    thumb.append_task("/var/www/tornado/test2.jpg","http://google.com")
-#    thumb.append_task("/var/www/tornado/test3.jpg","http://takumus.com")
+    if(hashlib.md5(string.encode('utf-8')).hexdigest() == NG_hash):
+      return False
+    else:
+      return True
